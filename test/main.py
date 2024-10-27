fe39b4a3c82a5434035522324d23951529aed787
EClinick <Ethan@Clinick.net>
2024-10-26 18:24:12 -0700

    Refactored function 'main' in local_repo\test\main.py

--- test/main.py
+++ test/main.py
@@ -3,13 +3,13 @@ from code import inefficient_function, slow_sum, slow_multiply
 
 def main():
     data = [1, 2, 3, 2, 1, 4, 5]
-    numbers = [1, 2, 3, 4, 5]
-    unique_data = sorted(list(set(data)))
-    total = sum(numbers)
-    product = eval('*'.join(map(str, numbers)))
-    print(f'Unique Data: {unique_data}')
-    print(f'Sum: {total}')
-    print(f'Product: {product}')
+    unique_data = sorted(set(data))
+    numbers = list(range(1, 6))
+    total_sum_numbers = sum(numbers)
+    product_numbers = eval('*'.join(map(str, numbers)))
+    print(
+        f'Unique Data: {unique_data}\nSum: {total_sum_numbers}\nProduct: {product_numbers}'
+        )
 
 
 if __name__ == '__main__':
